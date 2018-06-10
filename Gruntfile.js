4a2a4335d29fe0d455006d40bae6e3627405bafa
Václav Vracovský <vracovsky@shoptet.cz>
2018-06-10 21:01:12 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+    require("load-grunt-tasks")(grunt);
+
+    var pkg = grunt.file.readJSON('package.json');
+
+    grunt.initConfig({
+        sass: {
+            development: {
+                options: {
+                    style: 'nested',
+                    sourcemap: 'inline'
+                },
+                files: {
+                    'dist/css/main.css': '_scss/main.scss'
+                }
+            },
+            production: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/css/main.css': '_scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: [
+                    '_scss/*.scss',
+                    '_scss/modules/*.scss',
+                    '../scaffolding/shoptet/*.scss'
+                ],
+                tasks: ['sass:development'],
+                options: {
+                    livereload: 35729
+                }
+            },
+            js: {
+                files: [
+                    'js/*.js'
+                ],
+                tasks: ['uglify:development'],
+                options: {
+                    livereload: 35729
+                }
+            }
+        },
+        uglify: {
+            development: {
+                options: {
+                    mangle: false,
+                    compress: false
+                },
+                files: {
+                    'dist/js/build.js': pkg.jsFiles
+                }
+            },
+            production: {
+                options: {
+                    mangle: false,
+                    compress: true
+                },
+                files: {
+                    'dist/js/build.js': pkg.jsFiles
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['uglify-js', 'compile-css']);
+    grunt.registerTask('uglify-js', ['uglify:production']);
+    grunt.registerTask('compile-css', ['sass:production']);
+    grunt.registerTask('compile-css-dev', ['sass:development']);
+
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+# Shoptet "api" microsite template
+
+## Prerequisities
+
+- Node.js https://nodejs.org/en/
+- Sass https://sass-lang.com/
+
+## Usage
+
+TBU
+
+`$ npm install` - only for first time
+
+`$ grunt build` - build CSS & JS files
+
+`$ grunt` - enable watch mode for SCSS & JS files
