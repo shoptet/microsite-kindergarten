7adf6208fa63773dab14cc36481d6a99449ff4b1
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2018-07-06 11:54:38 +0200

    Responsive navigation script made pretty

--- _js/main.js
+++ _js/main.js
@@ -1,80 +1,85 @@
+(function($) {
+
+    $.fn.shpResponsiveNavigation = function() {
+
+        return this.each(function() {
+
+            var $this = $(this),   //this = div .responsive-nav
+                maxWidth,
+                visibleLinks,
+                hiddenLinks,
+                button;
+
+            setup($this);
+
+            function setup(resNavDiv) {
+                maxWidth = resNavDiv.width();
+
+                // check if menu is even visible before start
+                if(maxWidth > 0) {
+                    visibleLinks = resNavDiv.find(".visible-links");
+                    visibleLinks.find("li").each(function() {
+                        $(this).attr("data-width", $(this).width());
+                    });
+
+                    //hidden navigation
+                    if (!resNavDiv.find(".hidden-links").length) {
+                        resNavDiv.append('<button class="btn"><div class="fas fa-bars"></div></button><ul class="hidden-links hidden"></ul>');
+                    }
+                    hiddenLinks = resNavDiv.find(".hidden-links");
+                    button = resNavDiv.find("button");
+
+                    //calculate visible links
+                    update(resNavDiv);
+                }
+            }
+
+            function update(resNavDiv) {
+                maxWidth = resNavDiv.width();
+
+                if(visibleLinks.width() > maxWidth) {
+                    button.show();
+                    var filledSpace = button.width();
+
+                    // push excess to hidden links
+                    visibleLinks.find('li').each(function(index) {
+                        filledSpace += $(this).data("width");
+                        if (filledSpace >= maxWidth) {
+                            $(this).appendTo(hiddenLinks);
+                        }
+                    });
+
+
+                } else {
+                    filledSpace = visibleLinks.width() + button.width();
+
+                    // push missing to visible links
+                    hiddenLinks.find('li').each(function(index) {
+                        filledSpace += $(this).data("width");
+                        if (filledSpace < maxWidth) {
+                            $(this).appendTo(visibleLinks);
+                        }
+                    });
+
+                    if (hiddenLinks.children("li").length == 0) {
+                        button.hide();
+                    }
+                }
+            }
+
+            $(window).resize(function() {
+                update($this);
+            });
+
+            $(button).click(function() {
+                hiddenLinks.toggleClass('hidden');
+            });
+        });
+    };
+
+})(jQuery);
+
 $(document).ready(function(){
-
-    /* RESPONSIVE MENU .RESPONSIVE-NAV SCRIPT START */
-    var moreMenuDropdown = '<button class="btn"><div class="fas fa-bars"></div></button><ul class="hidden-links hidden"></ul>';
-
-    $('body').find('.responsive-nav').each(function() {
-        $(this).append(moreMenuDropdown);
-    });
-
-    var menus = {};
-
-    $('body').find('.responsive-nav').each(function(index, value) {
-        menus[index] = value;
-        menus[index]['nav'] = $(this);
-        menus[index]['btn'] = $("button", this);
-        menus[index]['vlinks'] = $(".visible-links", this);
-        menus[index]['hlinks'] = $(".hidden-links", this);
-        menus[index]['breaks'] = [];
-
-        menus[index]['btn'].on('click', function() {
-            menus[index]['hlinks'].toggleClass('hidden');
-        });
-
-        updateNav(index);
-    });
-
-    function updateNav(index) {
-        // Count visible menus only
-        if(menus[index]['nav'].width() > 0) {
-            var availableSpace = menus[index]['btn'].hasClass('hidden') ? menus[index]['nav'].width() : menus[index]['nav'].width() - menus[index]['btn'].width() - 40;
-
-            // The visible list IS overflowing the nav
-            if(menus[index]['vlinks'].width() > availableSpace) {
-
-                // Record the width of the list
-                menus[index]['breaks'].push(menus[index]['vlinks'].width());
-
-                // Move item to the hidden list
-                menus[index]['vlinks'].children().last().prependTo(menus[index]['hlinks']);
-
-                // Show the dropdown btn
-                if(menus[index]['btn'].hasClass('hidden')) {
-                    menus[index]['btn'].removeClass('hidden');
-                }
-
-            // The visible list IS NOT overflowing
-            } else {
-
-                // There is space for another item in the nav
-                var breaksPosition = menus[index]['breaks'].length - 1;
-                if(availableSpace > menus[index]['breaks'][breaksPosition]) {
-
-                    // Move the item to the visible list
-                    menus[index]['hlinks'].children().first().appendTo(menus[index]['vlinks']);
-                    menus[index]['breaks'].pop();
-                }
-
-                // Hide the dropdown btn if hidden list is empty
-                if(menus[index]['breaks'].length < 1) {
-                    menus[index]['btn'].addClass('hidden');
-                    menus[index]['hlinks'].addClass('hidden');
-                }
-            }
-
-            // Recursive if the visible list is still overflowing the nav
-            if(menus[index]['vlinks'].width() > availableSpace) {
-                updateNav(index);
-            }
-        }
-    }
-
-    $(window).resize(function() {
-        $('body').find('.responsive-nav').each(function(index, value) {
-            updateNav(index);
-        });
-    });
-
-    /* RESPONSIVE MENU .RESPONSIVE-NAV SCRIPT END */
-
+    $(".responsive-nav").shpResponsiveNavigation();
 });
+
